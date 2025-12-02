0573d1e9bc6e180bc64fef54e397470e60812a10
Advikkhandelwal <advikkhandelwal1207@gmail.com>
2025-12-02 16:25:05 +0530

    replace auth.js code with the original one

--- routes/auth.js
+++ routes/auth.js
@@ -10,5 +10,9 @@ router.get('/login/status', validateToken, authController.login_status_get)
 
 router.post('/login', authController.login_post)
 
+router.get('/', (req, res) => {
+  res.send('Please read documentation for the API. (auth)')
+})
+
 // Export -----
 module.exports = router
